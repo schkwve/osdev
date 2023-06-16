9f196a90c83562691415d99b4efc6a8e7b302f9e
Jozef Nagy <schkwve@gmail.com>
2023-06-16 12:11:48 +0200

    Added license header

--- corelib/stdint.h
+++ corelib/stdint.h
@@ -1,3 +1,22 @@
+/**
+ * OSDev
+ * Copyright (C) 2023 Jozef Nagy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef __STDINT_H_
 #define __STDINT_H_
 
@@ -23,6 +42,6 @@ typedef unsigned long		size_t;
 typedef long				ssize_t;
 
 typedef int64_t				intmax_t;
-typedef uint64_t				uintmax_t;
+typedef uint64_t			uintmax_t;
 
 #endif // __STDINT_H_
